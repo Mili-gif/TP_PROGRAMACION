c324455e17a7c4943fab18f59a9c4bcfa833ad0e
javila3004 <104645424+javila3004@users.noreply.github.com>
2024-09-29 18:21:32 -0300

    nuevo archivo subido

---
+++ calcular_hora_min_segun.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SEGUNDOS_EN_UNA_HORA 3600
+#define MINUTOS_EN_UNA_HORA 60
+#define MAXIMO 86400
+
+
+
+int main ()
+{
+    int Segundos_Totales, Hora, Min, Seg_Restantes, Aux;
+    
+    printf("Bienvenido al programa\n");
+    do {
+        printf("Ingrese los segundos que quiera pasar a horas\n");
+        scanf("%d", &Segundos_Totales);
+        
+        while (Segundos_Totales <0 || Segundos_Totales > MAXIMO) {
+            printf("No es un valor valido, por favor ingreselo nuevamente\n");
+            scanf("%d", &Segundos_Totales);
+        }
+        if ( Segundos_Totales !=0) {
+            
+            Hora= (Segundos_Totales/SEGUNDOS_EN_UNA_HORA);
+            Aux=(Segundos_Totales % SEGUNDOS_EN_UNA_HORA);
+            Min=(Aux / MINUTOS_EN_UNA_HORA);
+            Seg_Restantes= (Aux % MINUTOS_EN_UNA_HORA);
+            printf("La cantidad de horas es: %d\n", Hora);
+            printf("La cantidad de minutos  es: %d\n", Min);
+            printf("La cantidad de segundos es: %d\n", Seg_Restantes);
+            printf("En caso de querer salir presione el 0\n");
+        }
+            
+    } while (Segundos_Totales !=0);
+    
+    return 0;
+}
